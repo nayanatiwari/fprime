e70f268a4aae75b058fec6862030330d769106f9
Nayana Tiwari <tiwarinyn@gmail.com>
2022-08-24 11:57:39 -0700

    fixed for cross-compile

--- cmake/toolchain/raspberrypi.cmake
+++ cmake/toolchain/raspberrypi.cmake
@@ -19,7 +19,8 @@ set(CMAKE_SYSTEM_PROCESSOR "arm")
 # Location of pi toolchain
 set(RPI_TOOLCHAIN "$ENV{RPI_TOOLCHAIN_DIR}")
 if ("${RPI_TOOLCHAIN}" STREQUAL "")
-    set(RPI_TOOLCHAIN "/opt/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf")
+	set(RPI_TOOLCHAIN "/usr/bin/")
+	#   set(RPI_TOOLCHAIN "/opt/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf")
 endif()
 # Check toolchain directory exists
 IF(NOT EXISTS "${RPI_TOOLCHAIN}")
@@ -27,8 +28,8 @@ IF(NOT EXISTS "${RPI_TOOLCHAIN}")
 endif()
 message(STATUS "Using RPI toolchain at: ${RPI_TOOLCHAIN}")
 # specify the cross compiler
-set(CMAKE_C_COMPILER "${RPI_TOOLCHAIN}/bin/arm-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "${RPI_TOOLCHAIN}/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_C_COMPILER "${RPI_TOOLCHAIN}/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "${RPI_TOOLCHAIN}/arm-linux-gnueabihf-g++")
 
 # where is the target environment
 set(CMAKE_FIND_ROOT_PATH  "${RPI_TOOLCHAIN}")
